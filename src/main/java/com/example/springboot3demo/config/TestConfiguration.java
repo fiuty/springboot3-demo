46c770e76f3e0e4263e87662c472ee2f748beb31
zdy <1125450960@qq.com>
2026-02-08 17:30:49 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot3demo/config/TestConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.springboot3demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @date: 2026/2/8 15:34
+ * @author: dd
+ */
+@Configuration
+public class TestConfiguration {
+    @Value("${test}")
+    private String test;
+}
